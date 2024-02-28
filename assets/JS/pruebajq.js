4e0a72368f9b4f73ce35557ac6138f9d5db4e937
Emperatriz <emperatrizfreites90@gmail.com>
2024-02-28 00:18:13 -0300

    se añade navbar y perfiles en grillas con bootstrap

---
+++ assets/JS/pruebajq.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    //Efectos
+    $( "button" ).on( "click", function() {
+        $( "h1" ).fadeToggle( "slow", "linear" );
+        
+    //Color   
+        $("p").css({"background-color": "green"});
+        $("#div1").css({"background-color":"blue"});
+    
+    //Animación    
+    $("#boton1").click(function(){
+        $( "#div1" ).animate({
+        height: "toggle"
+        }, 1000, function() {
+            });
+        } );
+    });
+});
